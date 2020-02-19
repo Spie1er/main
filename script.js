ae9405e7e90352d9755e2420c6712488a8c8de3e
Spie1er <berulava@gmail.com>
2020-02-19 23:08:21 +0400

    Changing default bg colors

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
 var body = document.getElementById("gradient");
 
-function changeBGC() {
-  body.style.background =
-  "linear-gradient(to right, "
-   + color1.value
-   + ", "
-   + color2.value
-   + ")";
-
-  css.textContent = body.style.background + ";";
-  }
+function changeColor() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";"
+}
 
 
+color1.addEventListener("input", changeColor)
 
-
-color1.addEventListener("input", changeBGC);
-color2.addEventListener("input",changeBGC);
+color2.addEventListener("input", changeColor)
